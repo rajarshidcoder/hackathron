30ccb01f8a3c90eefaf9ca4951df681ff95f35e5
Rajarshi Mondal <rajarshidgamer@gmail.com>
2023-09-17 00:46:33 +0530

    update_draft

--- app.js
+++ app.js
@@ -17,16 +17,16 @@ function openWeb() {
     const condition = document.getElementById('location_given').value;
 
     // Check if the condition is met
-    if (condition === 'shop') {
-        const url1 = 'Shop/index.html';
+    if (condition === 'bishnupur') {
+        const url1 = 'bishnupur/index.html';
         window.location.href = url1; // Open in the same tab
-    } else if (condition === 'condition2') {
-        const url2 = 'https://www.example2.com';
+    } else if (condition === 'shantiniketan') {
+        const url2 = 'shantiniketan/index.html';
         window.location.href = url2; // Open in the same tab
     } else if (condition === 'condition3') {
         const url3 = 'https://www.example3.com';
         window.location.href = url3; // Open in the same tab
     } else {
-        alert('No matching condition found. Website will not be opened.');
+        alert('No match found. (STILL IN DEVELOPMENT)');
     }
 }
